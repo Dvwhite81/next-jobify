df61e3ba8295e1b86812cb8abd7f445555509e53
Dvwhite81 <dvwhite1981@gmail.com>
2024-08-05 20:31:04 -0400

    Complete

--- components/JobsList.tsx
+++ components/JobsList.tsx
@@ -2,34 +2,43 @@
 import { useSearchParams } from 'next/navigation';
 import { useQuery } from '@tanstack/react-query';
 import { getAllJobsAction } from '@/utils/actions';
+import ComplexButtonContainer from './ComplexButtonContainer';
 import JobCard from './JobCard';
 
 export default function JobsList() {
   const searchParams = useSearchParams();
+
   const search = searchParams.get('search') || '';
   const jobStatus = searchParams.get('jobStatus') || 'all';
-  const page = Number(searchParams.get('page')) || 1;
+
+  const pageNumber = Number(searchParams.get('page')) || 1;
 
   const { data, isPending } = useQuery({
-    queryKey: ['jobs', search ?? '', jobStatus, page],
-    queryFn: () => getAllJobsAction({ search, jobStatus, page }),
+    queryKey: ['jobs', search ?? '', jobStatus, pageNumber],
+    queryFn: () => getAllJobsAction({ search, jobStatus, page: pageNumber }),
   });
   const jobs = data?.jobs || [];
+  const count = data?.count || 0;
+  const page = data?.page || 0;
+  const totalPages = data?.totalPages || 0;
 
-  if (isPending) {
-    return <h2 className="text-xl">Please Wait...</h2>;
-  }
+  if (isPending) return <h2 className="text-xl">Please Wait...</h2>;
 
-  if (jobs.length < 1) {
-    return <h2 className="text-xl">No Jobs Found...</h2>;
-  }
-
+  if (jobs.length < 1) return <h2 className="text-xl">No Jobs Found...</h2>;
   return (
     <>
-      <div className="grid md:grid-cols-2 gap-8">
-        {jobs.map((job) => (
-          <JobCard key={job.id} job={job} />
-        ))}
+      <div className="flex items-center justify-between mb-8">
+        <h2 className="text-xl font-semibold capitalize ">
+          {count} jobs found
+        </h2>
+        {totalPages < 2 ? null : (
+          <ComplexButtonContainer currentPage={page} totalPages={totalPages} />
+        )}
+      </div>
+      <div className="grid md:grid-cols-2  gap-8">
+        {jobs.map((job) => {
+          return <JobCard key={job.id} job={job} />;
+        })}
       </div>
     </>
   );
